604804ab9ef5946a34c05d3b79558d9a78f03b5d
Codder12346768 <dharmip060@gmail.com>
2023-10-06 10:41:48 +0530

    CSS and dataset updated

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
-import pandas as pd
 import random
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Load the DataFrame outside of the route function
-df = pd.read_csv("adjective_list.txt", sep=" ")
+# Load long sentences from the text file into a list with UTF-8 encoding
+with open("adjective_list1.txt", "r", encoding="utf-8") as file:
+    long_sentences = [line.strip() for line in file]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
-        
-        # Ensure that there are enough rows in the DataFrame to sample from
-        if len(df) >= 5:
-            adjectives = df.sample(5)['adjective'].tolist()
-        else:
-            adjectives = ["a", "b", "c", "d", "e"]  # Provide default values if there aren't enough adjectives
 
-        return render_template('result.html', name=name, adjectives=adjectives)
+        # Randomly select a long sentence
+        long_sentence = random.choice(long_sentences)
+
+        return render_template('result.html', name=name, sentence=long_sentence)
+
     return render_template('index.html')
 
 if __name__ == '__main__':
